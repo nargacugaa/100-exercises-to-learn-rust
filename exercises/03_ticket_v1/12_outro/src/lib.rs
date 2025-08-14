423c34b3da88b57283513a845db6341b6da361db
nargacugaa <15949180087@163.com>
2025-08-14 11:58:00 +0800

    Implement Order struct with validation and accessor methods

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,57 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    /// Creates a new [`Order`].
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        if product_name.is_empty() || product_name.len() > 300 {
+            panic!("error product");
+        }
+
+        if quantity <= 0 || unit_price <= 0 {
+            panic!("must greater than zero");
+        }
+
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+    
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+    
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+    
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+    
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.product_name = product_name;
+    }
+    
+    pub fn set_quantity(&mut self, quantity: u32) {
+        self.quantity = quantity;
+    }
+    
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        self.unit_price = unit_price;
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    } 
+}
